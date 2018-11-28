79ca7dc6abcc8f906c7b129343fcd5e3d59f1902
Sandeep Mistry <s.mistry@arduino.cc>
2018-11-28 16:27:00 -0500

    Add define for constants and comments

--- src/MKRENV.cpp
+++ src/MKRENV.cpp
@@ -8,6 +8,37 @@
 
 #define LIGHT_SENSOR_PIN A3
 
+#define HTS221_WHO_AM_I_REG        0x0f
+#define HTS221_CTRL1_REG           0x20
+#define HTS221_CTRL2_REG           0x21
+#define HTS221_STATUS_REG          0x27
+#define HTS221_HUMIDITY_OUT_L_REG  0x28
+#define HTS221_TEMP_OUT_L_REG      0x2a
+#define HTS221_H0_rH_x2_REG        0x30
+#define HTS221_H1_rH_x2_REG        0x31
+#define HTS221_T0_degC_x8_REG      0x32
+#define HTS221_T1_degC_x8_REG      0x33
+#define HTS221_T1_T0_MSB_REG       0x35
+#define HTS221_H0_T0_OUT_REG       0x36
+#define HTS221_H1_T0_OUT_REG       0x3a
+#define HTS221_T0_OUT_REG          0x3c
+#define HTS221_T1_OUT_REG          0x3e
+
+#define LPS25H_WHO_AM_I_REG        0x0f
+#define LPS25H_CTRL1_REG           0x20
+#define LPS25H_CTRL2_REG           0x21
+#define LPS25H_STATUS_REG          0x27
+#define LPS25H_PRESS_OUT_XL_REG    0x28
+#define LPS25H_PRESS_OUT_L_REG     0x29
+#define LPS25H_PRESS_OUT_H_REG     0x2a    
+
+#define VEML6075_UV_CONF_REG       0x00
+#define VEML6075_UVA_DATA_REG      0x07
+#define VEML6075_UVB_DATA_REG      0x09
+#define VEML6075_UVCOMP1_REG       0x0a
+#define VEML6075_UVCOMP2_REG       0x0b
+#define VEML6075_ID_REG            0x0c
+
 ENVClass::ENVClass(TwoWire  & wire, int lightSensorPin) :
   _wire(&wire),
   _lightSensorPin(lightSensorPin)
@@ -18,19 +49,19 @@ int ENVClass::begin()
 {
   _wire->begin();
 
-  if (i2cRead(HTS221_ADDRESS, 0x0f) != 0xbc) {
+  if (i2cRead(HTS221_ADDRESS, HTS221_WHO_AM_I_REG) != 0xbc) {
     end();
 
     return 0;
   }
 
-  if (i2cRead(LPS25H_ADDRESS, 0x0f) != 0xbd) {
+  if (i2cRead(LPS25H_ADDRESS, LPS25H_WHO_AM_I_REG) != 0xbd) {
     end();
 
     return 0;
   }
 
-  if (i2cReadWord(VEML6075_ADDRESS, 0x0c) != 0x0026) {
+  if (i2cReadWord(VEML6075_ADDRESS, VEML6075_ID_REG) != 0x0026) {
     end();
 
     return 0;
@@ -38,18 +69,24 @@ int ENVClass::begin()
 
   readHTS221Calibration();
 
-  i2cWrite(HTS221_ADDRESS, 0x20, 0x80);
-  i2cWrite(LPS25H_ADDRESS, 0x20, 0x80);
-  i2cWriteWord(VEML6075_ADDRESS, 0x00, 0x0010);
+  // enable HTS221 and LPS25H
+  i2cWrite(HTS221_ADDRESS, HTS221_CTRL1_REG, 0x80);
+  i2cWrite(LPS25H_ADDRESS, LPS25H_CTRL1_REG, 0x80);
+
+  // configure VEML6075 for 100 ms
+  i2cWriteWord(VEML6075_ADDRESS, VEML6075_UV_CONF_REG, 0x0010);
 
   return 1;
 }
 
 void ENVClass::end()
 {
-  i2cWriteWord(VEML6075_ADDRESS, 0x00, 0x0001);
-  i2cWrite(LPS25H_ADDRESS, 0x20, 0x00);
-  i2cWrite(HTS221_ADDRESS, 0x20, 0x00);
+  // shutdown VEML6075
+  i2cWriteWord(VEML6075_ADDRESS, VEML6075_UV_CONF_REG, 0x0001);
+
+  // disable HTS221 and LPS25H
+  i2cWrite(LPS25H_ADDRESS, LPS25H_CTRL1_REG, 0x00);
+  i2cWrite(HTS221_ADDRESS, HTS221_CTRL1_REG, 0x00);
 
   _wire->end();
 
@@ -59,76 +96,103 @@ void ENVClass::end()
 
 float ENVClass::readTemperature()
 {
-  i2cWrite(HTS221_ADDRESS, 0x21, 0x01);
+  // trigger one shot
+  i2cWrite(HTS221_ADDRESS, HTS221_CTRL2_REG, 0x01);
 
-  while ((i2cRead(HTS221_ADDRESS, 0x27) & 0x02) == 0) {
+  // wait for completion
+  while ((i2cRead(HTS221_ADDRESS, HTS221_STATUS_REG) & 0x01) == 0) {
     yield();
   }
 
-  int16_t tout = i2cRead16(HTS221_ADDRESS, 0x2a);
+  // read value and convert
+  int16_t tout = i2cRead16(HTS221_ADDRESS, HTS221_TEMP_OUT_L_REG);
 
   return (tout * _hts221TemperatureSlope + _hts221TemperatureZero);
 }
 
 float ENVClass::readHumidity()
 {
-  i2cWrite(HTS221_ADDRESS, 0x21, 0x01);
+  // trigger one shot
+  i2cWrite(HTS221_ADDRESS, HTS221_CTRL2_REG, 0x01);
 
-  while ((i2cRead(HTS221_ADDRESS, 0x27) & 0x02) == 0) {
+  // wait for completion
+  while ((i2cRead(HTS221_ADDRESS, HTS221_STATUS_REG) & 0x02) == 0) {
     yield();
   }
 
-  int16_t hout = i2cRead16(HTS221_ADDRESS, 0x28);
+  // read value and convert
+  int16_t hout = i2cRead16(HTS221_ADDRESS, HTS221_HUMIDITY_OUT_L_REG);
 
   return (hout * _hts221HumiditySlope + _hts221HumidityZero);
 }
 
 float ENVClass::readPressure()
 {
-  i2cWrite(LPS25H_ADDRESS, 0x21, 0x01);
+  // trigger one shot
+  i2cWrite(LPS25H_ADDRESS, LPS25H_CTRL2_REG, 0x01);
 
-  while ((i2cRead(LPS25H_ADDRESS, 0x27) & 0x02) == 0) {
+  // wait for completion
+  while ((i2cRead(LPS25H_ADDRESS, LPS25H_STATUS_REG) & 0x02) == 0) {
     yield();
   }
 
-  return (i2cRead(LPS25H_ADDRESS, 0x28) | (i2cRead(LPS25H_ADDRESS, 0x29) << 8) | (i2cRead(LPS25H_ADDRESS, 0x2a) << 16)) / 40960.0;
+  // read value and convert
+  return (i2cRead(LPS25H_ADDRESS, LPS25H_PRESS_OUT_XL_REG) | 
+          (i2cRead(LPS25H_ADDRESS, LPS25H_PRESS_OUT_L_REG) << 8) | 
+          (i2cRead(LPS25H_ADDRESS, LPS25H_PRESS_OUT_H_REG) << 16)) / 40960.0;
 }
 
 float ENVClass::readLux()
 {
+  // read analog value and convert to mV
   float mV = (analogRead(_lightSensorPin) * 3300.0) / 1023.0;
 
+  // 2 mV per lux
   return (mV / 2.0);
 }
 
+// UV formula's and constants based on:
+//   https://www.vishay.com/docs/84339/designingveml6075.pdf
+
 float ENVClass::readUVA()
 {
-  uint16_t uva = i2cReadWord(VEML6075_ADDRESS, 0x07);
-  uint16_t uvcomp1 = i2cReadWord(VEML6075_ADDRESS, 0x0a);
-  uint16_t uvcomp2 = i2cReadWord(VEML6075_ADDRESS, 0x0b);
+  const float a = 2.22;
+  const float b = 1.33;
 
-  float uvaComp = uva - (2.22 * uvcomp1) - (1.33 * uvcomp2);
+  // read UVA and UV COMP's, then calculate compensated value
+  uint16_t uva = i2cReadWord(VEML6075_ADDRESS, VEML6075_UVA_DATA_REG);
+  uint16_t uvcomp1 = i2cReadWord(VEML6075_ADDRESS, VEML6075_UVCOMP1_REG);
+  uint16_t uvcomp2 = i2cReadWord(VEML6075_ADDRESS, VEML6075_UVCOMP2_REG);
+
+  float uvaComp = uva - (a * uvcomp1) - (b * uvcomp2);
 
   return uvaComp;
 }
 
 float ENVClass::readUVB()
 {
-  uint16_t uvb = i2cReadWord(VEML6075_ADDRESS, 0x09);
-  uint16_t uvcomp1 = i2cReadWord(VEML6075_ADDRESS, 0x0a);
-  uint16_t uvcomp2 = i2cReadWord(VEML6075_ADDRESS, 0x0b);
+  const float c = 2.95;
+  const float d = 1.74;
 
-  float uvbComp = uvb - (2.95 * uvcomp1) - (1.74 * uvcomp2);
+  // read UVB and UV COMP's, then calculate compensated value
+  uint16_t uvb = i2cReadWord(VEML6075_ADDRESS, VEML6075_UVB_DATA_REG);
+  uint16_t uvcomp1 = i2cReadWord(VEML6075_ADDRESS, VEML6075_UVCOMP1_REG);
+  uint16_t uvcomp2 = i2cReadWord(VEML6075_ADDRESS, VEML6075_UVCOMP2_REG);
+
+  float uvbComp = uvb - (c * uvcomp1) - (d * uvcomp2);
 
   return uvbComp;
 }
 
 float ENVClass::readUVIndex()
 {
+  const float UVAresp = 0.001461;
+  const float UVBresp = 0.002591;
+
   float uva = readUVA();
   float uvb = readUVB();
 
-  float uvi = ((uva * 0.001461) + (uvb * 0.002591)) / 2.0;
+  float uvi = ((uva * UVAresp) + (uvb * UVBresp)) / 2.0;
 
   return uvi;
 }
@@ -190,17 +254,20 @@ int ENVClass::i2cWriteWord(uint8_t address, uint8_t reg, uint16_t val)
 
 void ENVClass::readHTS221Calibration()
 {
-  uint8_t h0rH = i2cRead(HTS221_ADDRESS, 0x30);
-  uint8_t h1rH = i2cRead(HTS221_ADDRESS, 0x31);
+  uint8_t h0rH = i2cRead(HTS221_ADDRESS, HTS221_H0_rH_x2_REG);
+  uint8_t h1rH = i2cRead(HTS221_ADDRESS, HTS221_H1_rH_x2_REG);
 
-  uint16_t t0degC = i2cRead(HTS221_ADDRESS, 0x32) | ((i2cRead(HTS221_ADDRESS, 0x35) & 0x03) << 8);
-  uint16_t t1degC = i2cRead(HTS221_ADDRESS, 0x33) | ((i2cRead(HTS221_ADDRESS, 0x35) & 0x0c) << 6);
+  uint16_t t0degC = i2cRead(HTS221_ADDRESS, HTS221_T0_degC_x8_REG) | ((i2cRead(HTS221_ADDRESS, HTS221_T1_T0_MSB_REG) & 0x03) << 8);
+  uint16_t t1degC = i2cRead(HTS221_ADDRESS, HTS221_T1_degC_x8_REG) | ((i2cRead(HTS221_ADDRESS, HTS221_T1_T0_MSB_REG) & 0x0c) << 6);
 
-  int16_t h0t0Out = i2cRead16(HTS221_ADDRESS, 0x36);
-  int16_t h1t0Out = i2cRead16(HTS221_ADDRESS, 0x3a);
+  int16_t h0t0Out = i2cRead16(HTS221_ADDRESS, HTS221_H0_T0_OUT_REG);
+  int16_t h1t0Out = i2cRead16(HTS221_ADDRESS, HTS221_H1_T0_OUT_REG);
 
-  int16_t t0Out = i2cRead16(HTS221_ADDRESS, 0x3c);
-  int16_t t1Out = i2cRead16(HTS221_ADDRESS, 0x3e);
+  int16_t t0Out = i2cRead16(HTS221_ADDRESS, HTS221_T0_OUT_REG);
+  int16_t t1Out = i2cRead16(HTS221_ADDRESS, HTS221_T1_OUT_REG);
+
+  // calculate slopes and 0 offset from calibration values,
+  // for future calculations: value = a * X + b
 
   _hts221HumiditySlope = (h1rH - h0rH) / (2.0 * (h1t0Out - h0t0Out));
   _hts221HumidityZero = (h0rH / 2.0) - _hts221HumiditySlope * h0t0Out;
